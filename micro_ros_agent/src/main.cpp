4af30e6fd1815197e85b3445c57714a3ab1c50f5
Pablo Garrido <pablogs9@gmail.com>
2020-08-04 07:53:51 +0200

    Initial changes

--- micro_ros_agent/src/main.cpp
+++ micro_ros_agent/src/main.cpp
@@ -1,87 +1,43 @@
-// Copyright 2017-present Proyectos y Sistemas de Mantenimiento SL (eProsima).
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 #include <uxr/agent/utils/CLI.hpp>
+#include <uxr/agent/types/XRCETypes.hpp>
+#include <uxr/agent/utils/Callbacks.hpp>
 #include <csignal>
+
+#include "graph_manager.hpp"
+
+void printGUID(const dds::GUID_t& guid) {
+    std::cout <<  "GUID:\n\t entityId.entityKind: " << (int) guid.entityId().entityKind() << "\n";
+    std::cout <<  "\t entityId.m_entityKey: ";
+
+    for (size_t i = 0; i < 3; i++){
+        std::cout << (int) guid.entityId().entityKey()[i] << " ";
+    }
+
+    std::cout <<  "\n\t guidPrefix: ";
+
+    for (size_t i = 0; i < 12; i++){
+        std::cout << (int) guid.guidPrefix()[i] << " ";
+    }
+    std::cout <<  "\n";
+}
+
+void on_create_callback(const dds::xrce::ObjectKind& kind, const dds::GUID_t& guid) {
+  std::cout <<  "ObjectKind: " <<(int) kind << "\n";
+  printGUID(guid);
+}
 
 int main(int argc, char** argv)
 {
-#ifndef _WIN32
-    sigset_t signals;
-    sigemptyset(&signals);
-    if(sigaddset(&signals, SIGINT) && sigaddset(&signals, SIGTERM))
-    {
-        std::cerr << "Wrong signalset" << std::endl;
-        std::exit(EXIT_FAILURE);
-    }
-    sigprocmask( SIG_BLOCK, &signals, nullptr );
-#endif
+    eprosima::uxr::UDPv4Agent agent_udp4(8888, eprosima::uxr::Middleware::Kind::FASTDDS);
+    agent_udp4.set_verbose_level(5);
 
+    auto * cb_a = on_create_callback;
+    agent_udp4.add_oncreate_callback(cb_a);
+    
+    agent_udp4.start();
 
-    /* CLI application. */
-    CLI::App app("micro-ROS Agent");
-    app.require_subcommand(1, 1);
-    app.get_formatter()->column_width(42);
+    graphmanager::GraphManager * gm = new graphmanager::GraphManager();
+    while (true) { std::this_thread::sleep_for(std::chrono::seconds(1)); }
 
-    /* CLI subcommands. */
-    eprosima::uxr::cli::UDPv4Subcommand udpv4_subcommand(app);
-    eprosima::uxr::cli::UDPv6Subcommand udpv6_subcommand(app);
-    eprosima::uxr::cli::TCPv4Subcommand tcpv4_subcommand(app);
-    eprosima::uxr::cli::TCPv6Subcommand tcpv6_subcommand(app);
-#ifndef _WIN32
-    eprosima::uxr::cli::TermiosSubcommand serial_subcommand(app);
-    eprosima::uxr::cli::PseudoTerminalSubcommand pseudo_serial_subcommand(app);
-#endif
-    eprosima::uxr::cli::ExitSubcommand exit_subcommand(app);
-
-    /* CLI parse. */
-    std::string cli_input{};
-    for (int i = 1; i < argc; ++i)
-    {
-        cli_input.append(argv[i]);
-        cli_input.append(" ");
-    }
-
-    while (true)
-    {
-        try
-        {
-            app.parse(cli_input);
-            break;
-        }
-        catch (const CLI::ParseError& e)
-        {
-            app.exit(e);
-            std::cin.clear();
-            std::cout << std::endl;
-            std::cout << "Enter command: ";
-            std::getline(std::cin, cli_input);
-        }
-    }
-
-#ifdef  _WIN32
-    /* Waiting until exit. */
-    std::cin.clear();
-    char exit_flag = 0;
-    while ('q' != exit_flag)
-    {
-        std::cin >> exit_flag;
-    }
-#else
-    /* Wait for SIGTERM/SIGINT instead, as reading from stdin may be redirected to /dev/null. */
-    int n_signal = 0;
-    sigwait(&signals, &n_signal);
-#endif
     return 0;
 }
